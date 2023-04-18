ef6b5f3afc6037b12f4de82e6e3bba4b73acd8f2
Yann Kamche <yannkamche@gmail.com>
2023-04-18 18:16:26 +0100

    type casting

--- Type Casting/typeCasting.c
+++ Type Casting/typeCasting.c
@@ -5,7 +5,7 @@ int main() {
     int numberOfPizza = 17;
     int people = 2;
 
-    double halfThePizza = (double) numberOfPizza / people;
+    double halfThePizza = (double) (numberOfPizza / people);
     printf("%f", halfThePizza);
     return 0;
 }
