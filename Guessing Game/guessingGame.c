ef3d40ffdffd6d64181ff1abf03c12ce2e18a0ab
Yann Kamche <yannkamche@gmail.com>
2023-04-23 17:25:18 +0100

    Simple guessing game using function calls

---
+++ Guessing Game/guessingGame.c
@@ -0,0 +1,51 @@
+// A guessing Game
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+//function declaration
+char getUserName();
+void guessingGame();
+int getUserGuess(int);
+void checkGuess(int, int);
+int rangeGuess();
+
+int main() 
+{   
+    srand(time(NULL));
+    guessingGame();
+    return 0;
+}
+
+//function definition
+char getUserName() {
+    printf("Welcome to the World of fun!!!\nEnter your name: ");
+    char userName[20];
+    scanf("%s", userName);
+}
+int getUserGuess(int range) {
+    printf("Guess a Number from 0 to %d: ", range);
+    int guess;
+    scanf("%d", &guess);
+    return guess;
+}
+void checkGuess(int random, int guess) {
+    if(random == guess)
+        printf("Good guess!!!");
+    else
+        printf("Try Again later!!");
+}
+int rangeGuess() {
+    printf("Choose your range of numbers to guess: ");
+    int maxValue;
+    scanf("%d", &maxValue);
+    return maxValue;
+}
+void guessingGame() {
+    getUserName();
+    int random, maxValue;
+    maxValue = rangeGuess();
+    random = rand() % maxValue + 1;
+    printf("%d\n", random);
+    checkGuess(random, getUserGuess(maxValue));
+}
